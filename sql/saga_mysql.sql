603675544ff4ab541706cebf7c0bf22445b61bd7
yusys-cloud <yangzq80@gmail.com>
2023-04-27 11:08:07 +0800

    doc(readme):modify readme.md

--- sql/saga_mysql.sql
+++ sql/saga_mysql.sql
@@ -1,24 +1,24 @@
-CREATE TABLE seata_state_inst (
-	id varchar(255) NOT NULL,
-	machine_inst_id varchar(255) NOT NULL,
-	name varchar(255) NOT NULL,
-	type varchar(255),
-	service_name varchar(255),
-	service_method varchar(255),
-	service_type varchar(255),
-	business_key varchar(255),
-	state_id_compensated_for varchar(255),
-	state_id_retried_for varchar(255),
-	gmt_started timestamp NOT NULL,
-	is_for_update tinyint(1),
-	input_params longtext,
-	output_params longtext,
-	status varchar(255) NOT NULL,
-	excep longblob,
-	gmt_updated timestamp,
-	gmt_end timestamp,
-	PRIMARY KEY (id)
-) ENGINE = InnoDB CHARSET = utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE TABLE `seata_state_inst` (
+  `id` varchar(255) NOT NULL,
+  `machine_inst_id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `service_name` varchar(255) DEFAULT NULL,
+  `service_method` varchar(255) DEFAULT NULL,
+  `service_type` varchar(255) DEFAULT NULL,
+  `business_key` varchar(255) DEFAULT NULL,
+  `state_id_compensated_for` varchar(255) DEFAULT NULL,
+  `state_id_retried_for` varchar(255) DEFAULT NULL,
+  `gmt_started` timestamp NOT NULL,
+  `is_for_update` tinyint(1) DEFAULT NULL,
+  `input_params` longtext,
+  `output_params` longtext,
+  `status` varchar(255) NOT NULL,
+  `excep` longblob,
+  `gmt_updated` timestamp NULL DEFAULT NULL,
+  `gmt_end` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE seata_state_machine_def (
 	id varchar(255) NOT NULL,
